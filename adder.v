cc2c191959ea6e5bc7ba0b1e01b12b3f03571e02
nguyen <nguyenpv195@gmail.com>
2019-12-19 09:29:21 +0700

    transmission

--- adder.v
+++
@@ -1,4 +0,0 @@
-module adder(clk1, input1, input2, out);
-input clk1, input1, input2;
-output out;
-endmodule
--- mixer.v
+++
@@ -1,9 +0,0 @@
-module mixer(clk, lo, bb, mixer_out);
-  input clk;
-  input [7:0] lo, bb;
-  output reg [15:0] mixer_out;
-  always @(posedge clk)
-  begin
-    mixer_out = (lo - 100)*(bb - 100) + 10000;
-  end
-endmodule
