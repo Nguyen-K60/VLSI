fbe34a1e5c41fac60d1c6ad026e5b3d14089260c
nguyen <nguyenpv195@gmail.com>
2019-12-31 11:22:52 +0700

    m

--- Modulate.v
+++ Modulate.v
@@ -1,6 +1,6 @@
 module Modulate(clk, clk1, QPSK_out);
   input clk, clk1;  
-  output reg signed [9:0] QPSK_out;
+  output wire signed [9:0] QPSK_out;
   wire Ichannel, Qchannel; 
   wire signed [9:0] sb00, sb01, sb11, sb10;
   GenerateIAndQ gen(.clk1(clk1), .Ichannel(Ichannel), .Qchannel(Qchannel));
@@ -8,5 +8,5 @@ module Modulate(clk, clk1, QPSK_out);
   Symbol01 sym01(.clk(clk), .Symbol01_out(sb01));
   Symbol11 sym11(.clk(clk), .Symbol11_out(sb11));
   Symbol10 sym10(.clk(clk), .Symbol10_out(sb10));
-  GetQPSKOutput(.clk(clk), .sb00(sb00), .sb01(sb01), .sb11(sb11), .sb10(sb10), .Ichannel(Ichannel), .Qchannel(Qchannel), .QPSK_out(QPSK_out));
+  GetQPSKOutput qpskout(.clk(clk), .sb00(sb00), .sb01(sb01), .sb11(sb11), .sb10(sb10), .Ichannel(Ichannel), .Qchannel(Qchannel), .QPSK_out(QPSK_out));
 endmodule
