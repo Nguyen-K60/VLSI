c2e949ad74d6c1a44487f819b7b31c425d833620
nguyen <nguyenpv195@gmail.com>
2020-01-04 11:39:15 +0700

    av

--- Modulate.v
+++ Modulate.v
@@ -1,8 +1,9 @@
 module Modulate(clk, clk1, bit_stream, QPSK_out);
   input clk, clk1;
-  input bit_stream;  
+  output wire bit_stream;  
   output wire signed [9:0] QPSK_out;  
   wire Ichannel, Qchannel;  
+  GenerateInput gen(.clk1(clk1), .bit_stream(bit_stream));
   Split split(.clk1(clk1), .bit_stream(bit_stream), .Ichannel(Ichannel), .Qchannel(Qchannel));
   Mix mix(.clk(clk), .Ichannel(Ichannel), .Qchannel(Qchannel), .QPSK_out(QPSK_out));
 endmodule
--- top.v
+++ top.v
@@ -1,10 +1,7 @@
-module top(clk, clk1, bit_stream, QPSK_out, I_receive, Q_receive, I_filtered, Q_filtered, I_demodulate, Q_demodulate, bit_stream_demodulate);
+module top(clk, clk1, bit_stream, QPSK_out, bit_stream_demodulate);
   input clk, clk1;  
   output signed [9:0] QPSK_out;
-  output signed [18:0] I_receive, Q_receive, I_filtered, Q_filtered;
-  output I_demodulate, Q_demodulate;
   output wire bit_stream, bit_stream_demodulate; 
-  GenerateInput gen(.clk1(clk1), .bit_stream(bit_stream));
   Modulate modulate(.clk(clk), .clk1(clk1), .bit_stream(bit_stream), .QPSK_out(QPSK_out));
-  Demodulate demodulate(.clk(clk), .clk1(clk1), .QPSK_out(QPSK_out), .I_receive(I_receive), .Q_receive(Q_receive), .I_filtered(I_filtered), .Q_filtered(Q_filtered),.I_demodulate(I_demodulate), .Q_demodulate(Q_demodulate), .bit_stream_demodulate(bit_stream_demodulate));
+  Demodulate demodulate(.clk(clk), .clk1(clk1), .QPSK_out(QPSK_out), .bit_stream_demodulate(bit_stream_demodulate));
 endmodule
